0404edcd1fba31615229296e85c69587e5d39b05
Sérgio Bonani <sergiobonani@gmail.com>
2019-07-23 23:43:09 -0300

    Alterações/criação de controller, entidade e banco de dados h2

---
+++ src/main/java/com/sergiobonani/crudjavaionic/domain/Categoria.java
@@ -0,0 +1,57 @@
+package com.sergiobonani.crudjavaionic.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Categoria implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String nome;
+
+	public Categoria(){
+
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Categoria categoria = (Categoria) o;
+		return id.equals(categoria.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	public Categoria(Integer id, String nome){
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
--- src/main/java/com/sergiobonani/crudjavaionic/resources/CategoriaResource.java
+++ src/main/java/com/sergiobonani/crudjavaionic/resources/CategoriaResource.java
@@ -1,18 +1,28 @@
 package com.sergiobonani.crudjavaionic.resources;
 
+import com.sergiobonani.crudjavaionic.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String Listar(){
-		return "Rest Ok";
+	public List<Categoria> Listar(){
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		ArrayList<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 }
