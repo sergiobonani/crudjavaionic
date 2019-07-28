62b1f99efaf0d02fab837c4568dd4c3f34a45ce1
Sérgio Bonani <sergiobonani@gmail.com>
2019-07-28 13:34:21 -0300

    Adicionando POST, PUT, DELETE e GET para Categoria

--- src/main/java/com/sergiobonani/crudjavaionic/resources/CategoriaResource.java
+++ src/main/java/com/sergiobonani/crudjavaionic/resources/CategoriaResource.java
@@ -1,15 +1,20 @@
 package com.sergiobonani.crudjavaionic.resources;
 
 import com.sergiobonani.crudjavaionic.domain.Categoria;
+import com.sergiobonani.crudjavaionic.domain.Cliente;
+import com.sergiobonani.crudjavaionic.dto.CategoriaDTO;
 import com.sergiobonani.crudjavaionic.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.lang.reflect.Method;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Handler;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -19,11 +24,38 @@ public class CategoriaResource {
 	private CategoriaService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id){
-
-
-		Categoria categoria = service.buscar(id);
+	public ResponseEntity<Categoria> find(@PathVariable Integer id){
+		Categoria categoria = service.find(id);
 
 		return ResponseEntity.ok().body(categoria);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria){
+		categoria = service.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody Categoria categoria){
+		categoria.setId(id);
+		categoria = service.update(categoria);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> categorias = service.findAll();
+		List<CategoriaDTO> listDto = categorias.stream().map(c -> new CategoriaDTO(c)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/com/sergiobonani/crudjavaionic/services/CategoriaService.java
+++ src/main/java/com/sergiobonani/crudjavaionic/services/CategoriaService.java
@@ -1,11 +1,14 @@
 package com.sergiobonani.crudjavaionic.services;
 
 import com.sergiobonani.crudjavaionic.domain.Categoria;
+import com.sergiobonani.crudjavaionic.exceptions.DataIntegrityException;
 import com.sergiobonani.crudjavaionic.exceptions.ObjectNotFoundException;
 import com.sergiobonani.crudjavaionic.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,9 +16,31 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository;
 
-	public Categoria buscar(Integer id){
+	public Categoria find(Integer id){
 		Optional<Categoria> categoria = repository.findById(id);
 		return categoria.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria categoria){
+		categoria.setId(null);
+		return repository.save(categoria);
+	}
+
+	public Categoria update(Categoria categoria){
+		return repository.save(categoria);
+	}
+
+	public void delete(Integer id){
+		find(id);
+		try {
+			repository.deleteById(id);
+		}catch (DataIntegrityViolationException e){
+			throw  new DataIntegrityException("Não é possível excluir uma categoria vinculada a um produto");
+		}
+	}
+
+	public List<Categoria> findAll(){
+		return repository.findAll();
 	}
 }
