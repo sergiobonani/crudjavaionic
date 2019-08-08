bf233d089d3d5cad848c2016100cf7f641af9e61
Sérgio Bonani <sergiobonani@gmail.com>
2019-08-07 23:38:39 -0300

    Profile de teste

---
+++ src/main/java/com/sergiobonani/crudjavaionic/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.sergiobonani.crudjavaionic.config;
+
+import com.sergiobonani.crudjavaionic.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Bean
+	public boolean instatiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
