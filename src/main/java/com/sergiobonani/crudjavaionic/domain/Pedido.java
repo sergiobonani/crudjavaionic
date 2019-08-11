8373c4ed43baf3030a69ae8962db90e98596a8fd
Sérgio Bonani <sergiobonani@gmail.com>
2019-08-11 19:44:25 -0300

    Implementação toString de pedido

--- src/main/java/com/sergiobonani/crudjavaionic/domain/Pedido.java
+++ src/main/java/com/sergiobonani/crudjavaionic/domain/Pedido.java
@@ -5,10 +5,9 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Entity
 public class Pedido implements Serializable {
@@ -112,5 +111,31 @@ public class Pedido implements Serializable {
 		}
 
 		return soma;
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("Pedido número: ");
+		builder.append(getId());
+		builder.append(", Instante: ");
+		builder.append(sdf.format(getInstante()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do pagamento: ");
+		builder.append(getPagamento().getEstadoPagamento().getDescricao());
+		builder.append("\nDetalhes:\n");
+
+		for (ItemPedido itens: getItens()){
+			builder.append(itens.toString());
+		}
+
+		builder.append("Valor total: ");
+		builder.append(nf.format(getValorTotal()));
+
+		return builder.toString();
 	}
 }
